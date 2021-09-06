112af8260b5f123ace7deec257f484963ec2f2bf
Anderson <andersonfonseca1999@hotmail.com>
2021-09-06 11:33:05 -0300

    Add articles and create state

--- first-challenges/src/app.tsx
+++ first-challenges/src/app.tsx
@@ -1,17 +1,73 @@
+import { useState } from 'react';
+
 import { Header } from './Header';
 import { Menu } from './Menu';
 import { Sidebar } from './Sidebar';
 import { Content } from './Content';
 import { Footer } from './Footer';
+import { Button } from './Button';
+
+const articles = [
+  {
+    id: 1,
+    title: 'Messi no PSG',
+    content: (
+      <div>
+        <h3>Entenda como a chegada de Lionel Messi muda o vestiário do PSG</h3>
+        <p>
+          Segundo o "L'Equipe", o argentino tem impressionado diversos membros
+          do clube em apenas uma semana de treinamentos. Há, inclusive, atletas
+          que duvidavam sobre uma permanência no clube e que atualmente
+          acreditam na conquista da Champions League.
+        </p>
+
+        <Button kind='primary'>Saiba mais</Button>
+      </div>
+    ),
+  },
+  {
+    id: 2,
+    title: 'Bolo de Cenoura',
+    content: (
+      <div>
+        <ul>
+          <li>1/2 xícara (chá) de óleo</li>
+          <li>3 cenouras médias raladas</li>
+          <li>4 ovos</li>
+          <li>2 xícaras (chá) de açúcar</li>
+          <li>2 e 1/2 xícaras (chá) de farinha de trigo</li>
+          <li>1 colher (sopa) de fermento em pó</li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    id: 3,
+    title: 'Apenas uma ferrrari',
+    content: (
+      <div>
+        <img
+          src='https://images.unsplash.com/photo-1592198084033-aade902d1aae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+          alt='Ferrari'
+        />
+      </div>
+    ),
+  },
+];
 
 export function App() {
+  const [article, setArticle] = useState({
+    title: articles[0].title,
+    content: articles[0].content,
+  });
+
   return (
     <>
       <Header />
       <Menu />
       <div className='box'>
-        <Sidebar />
-        <Content />
+        <Sidebar articles={articles} setArticle={setArticle} />
+        <Content article={article} />
       </div>
       <Footer />
     </>
