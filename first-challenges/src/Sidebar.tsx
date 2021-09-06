01a37b5d15b717cee5ec10e19de6c593a48e4ec8
Anderson <andersonfonseca1999@hotmail.com>
2021-09-06 11:34:55 -0300

    Add links to articles in the Sidebar

--- first-challenges/src/Sidebar.tsx
+++ first-challenges/src/Sidebar.tsx
@@ -1,3 +1,35 @@
-export function Sidebar() {
-  return <aside className='sidebar'>Sidebar</aside>;
+type ArticlesProps = {
+  id: number;
+  title: string;
+  content: JSX.Element;
+};
+
+type SetArticle = Omit<ArticlesProps, 'id'>;
+
+type SidebarProps = {
+  articles: ArticlesProps[];
+  setArticle: React.Dispatch<React.SetStateAction<SetArticle>>;
+};
+
+export function Sidebar({ articles, setArticle }: SidebarProps) {
+  return (
+    <aside className='sidebar'>
+      {articles.map((article) => (
+        // eslint-disable-next-line jsx-a11y/anchor-is-valid
+        <a
+          className='article-link'
+          href='#'
+          key={article.id}
+          onClick={() =>
+            setArticle({
+              title: article.title,
+              content: article.content,
+            })
+          }
+        >
+          {article.title}
+        </a>
+      ))}
+    </aside>
+  );
 }
