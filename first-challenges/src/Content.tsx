4d9d440d14c97e6616540ea4ce3dcc08e50689b5
Anderson <andersonfonseca1999@hotmail.com>
2021-09-06 11:38:35 -0300

    Add article to Content

--- first-challenges/src/Content.tsx
+++ first-challenges/src/Content.tsx
@@ -1,45 +1,19 @@
-import { Button } from './Button';
-import { H2 } from './H2';
+type ArticleProps = {
+  title: string;
+  content: React.ReactNode;
+};
 
-export function Content() {
+type ContentProps = {
+  article: ArticleProps;
+};
+
+export function Content({ article }: ContentProps) {
   return (
     <main className='content'>
       <section className='section'>
-        <H2>Lorem Ipsum</H2>
+        <h2>{article.title}</h2>
 
-        <p className='paragraph'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada
-          dolor in ipsum lobortis, ac facilisis justo rutrum. Cras eu dui
-          pharetra, rhoncus augue euismod, pulvinar orci. Mauris id bibendum mi,
-          tristique interdum elit. Nulla facilisi. Suspendisse in cursus enim.
-          Nam vitae accumsan lectus.
-        </p>
-
-        <p className='paragraph'>
-          Cras in cursus ante. Nulla facilisi. Nulla hendrerit ultricies ipsum
-          facilisis interdum. Nullam quis arcu ultricies augue luctus facilisis.
-          In metus quam, fermentum suscipit gravida ut, elementum non neque.
-          Aenean mollis purus sit amet urna scelerisque, sed semper nulla
-          tincidunt. Cras egestas dui maximus urna dapibus consequat at nec
-          metus. Quisque fringilla nisi non augue porta, et consectetur lacus
-          ultrices. Suspendisse a lorem eleifend, luctus ante ac, cursus enim.
-          Duis eu congue diam. In ante ex, hendrerit vitae eros id, elementum
-          commodo nibh. Duis semper neque in ante euismod facilisis.
-        </p>
-
-        <p className='paragraph'>
-          Pellentesque a elit tincidunt orci dignissim condimentum vitae et
-          orci. Phasellus fringilla at justo et viverra. Vivamus iaculis, risus
-          convallis aliquet pretium, nisl metus consequat risus, eget tempus dui
-          ipsum vitae lorem. Pellentesque turpis tortor, efficitur nec lacus
-          vel, dictum condimentum velit. Interdum et malesuada fames ac ante
-          ipsum primis in faucibus. Phasellus rutrum cursus tortor ac sodales.
-          Donec auctor ipsum pulvinar tellus convallis finibus. Cras id mauris
-          lacus. Nulla consectetur, metus quis volutpat auctor, augue mauris
-          ullamcorper justo, nec tincidunt magna tellus quis ipsum.
-        </p>
-
-        <Button kind='primary'>Know more</Button>
+        {article.content}
       </section>
     </main>
   );
