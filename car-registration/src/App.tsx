1542f3ba2f9d806e541babe06384230006472250
Anderson <andersonfonseca1999@hotmail.com>
2021-09-11 13:57:16 -0300

    Add components to App.tsx

--- car-registration/src/App.tsx
+++ car-registration/src/App.tsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
+import Form from './components/Form';
+import Table from './components/Table';
+
 function App() {
-  return <div></div>;
+  const [updateTable, setUpdateTable] = useState(false);
+
+  return (
+    <>
+      <Form setUpdateTable={setUpdateTable} />
+      <Table updateTable={updateTable} setUpdateTable={setUpdateTable} />
+    </>
+  );
 }
 
 export default App;
